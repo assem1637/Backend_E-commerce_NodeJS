f4e21926a4736e2e450253cba1451ef4abf0fe64
Assem Saeed <saeedassem1637@gmail.com>
2023-02-24 14:46:13 +0200

    update product

--- src/Components/product/product.service.js
+++ src/Components/product/product.service.js
@@ -2,7 +2,7 @@ import productModel from "./product.model.js";
 import AppError from '../../Utils/appErrors.js';
 import slugify from "slugify";
 import cloudinary from 'cloudinary';
-
+import ApiFeatures from "../../Utils/apiFeatures.js";
 
 
 
@@ -47,8 +47,16 @@ const ErrorHandler = (fun) => {
 
 export const getAllProducts = ErrorHandler(async (req, res, next) => {
 
-    const allProducts = await productModel.find({}).populate("category subcategory brand", "name");
-    res.status(200).json({ message: "Success", data: allProducts });
+    // MongooseQuery
+
+    let MongooseQuery = productModel.find({}).populate("category subcategory brand", "name");
+
+    let apiFeatures = new ApiFeatures(MongooseQuery, req.query).search().pagination().fields().sort().filter();
+
+
+    const allProducts = await apiFeatures.MongooseQuery;
+
+    res.status(200).json({ message: "Success", page: apiFeatures.page, data: allProducts });
 
 });
 
---
+++ src/Utils/apiFeatures.js
@@ -0,0 +1,114 @@
+class ApiFeatures {
+
+
+    constructor(MongooseQuery, QueryString) {
+
+        this.MongooseQuery = MongooseQuery;
+        this.QueryString = QueryString;
+
+    };
+
+
+    pagination() {
+
+        // 1- Pagination
+
+        let page = (this.QueryString.page * 1 > 0 ? this.QueryString.page * 1 : 1) || 1;
+        let limit = 5;
+        let skip = (page - 1) * limit;
+
+        this.MongooseQuery.skip(skip).limit(limit);
+        this.page = page;
+
+        return this;
+
+    };
+
+
+
+    filter() {
+
+        // 2- Filter
+
+        let Query = { ...this.QueryString };
+        let Query_Not_Use = ["page", "sort", "fields", "keyword"];
+
+        Query_Not_Use.forEach((ele) => {
+
+            delete Query[ele];
+
+        });
+
+
+        Query = JSON.stringify(Query);
+        Query = Query.replace(/(gte|gt|lte|lt)/g, (match) => `$${match}`);
+        Query = JSON.parse(Query);
+
+        this.MongooseQuery.find(Query);
+
+        return this;
+
+    };
+
+
+
+
+    sort() {
+
+
+        // 3- Sort
+
+        if (this.QueryString.sort) {
+
+            let value_of_query_sort = (this.QueryString.sort).split(",").join(" ");
+            this.MongooseQuery.sort(value_of_query_sort);
+
+        };
+
+
+        return this;
+
+    };
+
+
+
+    fields() {
+
+
+        // 4- Select
+
+        if (this.QueryString.fields) {
+
+            let value_of_query_fields = (this.QueryString.fields).split(",").join(" ");
+            this.MongooseQuery.select(value_of_query_fields);
+
+        };
+
+
+        return this;
+
+    };
+
+
+
+    search() {
+
+
+        // 5- Search
+
+        if (this.QueryString.keyword) {
+
+            this.MongooseQuery.find({ $or: [{ name: { $regex: this.QueryString.keyword, $options: "i" } }, { description: { $regex: this.QueryString.keyword, $options: "i" } }] });
+
+        };
+
+
+        return this;
+
+    };
+
+
+};
+
+
+export default ApiFeatures;

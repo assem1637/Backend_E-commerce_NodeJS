b3669d97f9447a94a3977841a03a2c72aeb58427
Assem Saeed <saeedassem1637@gmail.com>
2023-02-24 22:40:41 +0200

    update user1

---
+++ src/Components/user/user.model.js
@@ -0,0 +1,97 @@
+import mongoose from "mongoose";
+
+
+
+
+
+
+
+const userSchema = mongoose.Schema({
+
+    name: {
+
+        type: String,
+        required: [true, "Name Of User Is Required"],
+        trim: true,
+        minlength: [2, "2 Is Too Short Name Of User"],
+
+    },
+
+
+    age: {
+
+        type: Number,
+        min: [5, "5 Is The Lowest Age"],
+        max: [100, "100 Is The Largest Age"],
+        required: [true, "Age Of User Is Required"],
+
+    },
+
+
+    email: {
+
+        type: String,
+        required: [true, "Email Of User Is Required"],
+
+    },
+
+
+    password: {
+
+        type: String,
+        minlength: [6, "6 Is Too Short Password Of User"],
+        required: [true, "Password Of User Is Required"],
+
+    },
+
+
+    emailConfirm: {
+
+        type: Boolean,
+        default: false,
+
+    },
+
+
+    isActive: {
+
+        type: Boolean,
+        default: true,
+
+    },
+
+
+    phone: {
+
+        type: String,
+        required: [true, "Phone Of User Is Required"],
+
+    },
+
+
+    role: {
+
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+
+    },
+
+
+    profileImg: {
+
+        type: String,
+
+    },
+
+
+}, { timestamps: true });
+
+
+
+
+
+const userModel = mongoose.model("user", userSchema);
+
+
+export default userModel;
---
+++ src/Components/user/user.route.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { uploadSingleImage } from '../../Utils/uploadImage.js';
+import { getAllUsers, createUser, getSpecificUser, updateSpecificUser, deleteSpecificUser } from './user.service.js';
+import { signup, signin, confirmation } from './user.auth.js';
+
+
+
+
+
+const router = Router();
+
+
+
+router.route("/").get(getAllUsers).post(uploadSingleImage("profileImg"), createUser);
+router.route("/:id").get(getSpecificUser).put(uploadSingleImage("profileImg"), updateSpecificUser).delete(deleteSpecificUser);
+
+router.post("/signup", uploadSingleImage("profileImg"), signup);
+router.post("/signin", signin);
+router.post("/confirm/:token", confirmation);
+
+
+
+export default router;
